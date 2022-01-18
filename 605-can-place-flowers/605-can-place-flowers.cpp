0b5cb0077137edeede21edb0f9a39e827dcf836a
son0179 <48584203+son0179@users.noreply.github.com>
2022-01-18 14:16:08 +0900

    Time: 40 ms (6.78%), Space: 20.1 MB (99.61%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        // just index!!
+        if (n <= 0) return true;
+        if( flowerbed.size() == 1){
+            n -= !flowerbed[0];
+            if (n <= 0) return true;
+            else return false;
+        }
+        
+        if( !(flowerbed[0] || flowerbed[1]) ){
+            n--;
+            flowerbed[0]=1;
+            if (n <= 0) return true;
+        }
+        for (int i = 1; i < flowerbed.size()-1; i++) {  // i is index
+            if (!(flowerbed[i-1] || flowerbed[i] || flowerbed[i+1])) {
+                n--;
+                flowerbed[i]=1;
+                if (n <= 0) return true;
+            }
+        }
+        n -= !(flowerbed[flowerbed.size()-2 ] || flowerbed[flowerbed.size()-1 ]);
+        if(n<=0) return true;
+        return false;
+    }
+};
